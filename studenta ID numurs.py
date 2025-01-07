cebdec20f83cff09c97bf4f35eb2c0346340506f
Jekaterina Jakovleva <jekaterina.jakovleva@edu.rtu.lv>
2025-01-07 11:35:50 +0000

    07.01.2025

--- studenta ID numurs.py
+++ studenta ID numurs.py
@@ -20,3 +20,25 @@ with open("people.csv", "r") as file:
 url = "https://emn178.github.io/online-tools/crc32.html"
 driver.get(url)
 time.sleep(2)
+
+find= driver.find_element(By.ID, "input")
+find.send_keys("Hunter Hahn")
+
+find = driver.find_element(By.ID,"output")
+temp=find.get_attribute("value")
+
+wb = load_workbook("salary.xlsx")
+sheet = wb.active
+
+total_salary = 0
+for rinda in range(2, sheet.max_row + 1):
+    code = sheet['A' + str(rinda)].value
+    salary = sheet['B' + str(rinda)].value
+    if code == temp:  
+        total_salary += int(salary)  
+
+print(total_salary)
+
+wb.close()
+
+driver.quit()
